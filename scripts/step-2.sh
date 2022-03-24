d13414a52f2a35b999c3758bb9166b09c3870fa7
Asra Ali <asraa@google.com>
2022-03-24 14:30:43 -0500

    Add user prompts for sk
    Signed-off-by: Asra Ali <asraa@google.com>

--- scripts/step-2.sh
+++ scripts/step-2.sh
@@ -21,8 +21,15 @@ git checkout main
 git pull upstream main
 git status
 
+
+# Ask user to insert key 
+read -n1 -r -p -s "Insert your Yubikey, then press any key to continue..." 
+
 # Sign the root and targets with hardware key
 ./tuf sign -repository $REPO -roles root -roles targets -sk
+
+# Ask user to remove key (and replace with SSH security key)
+read -n1 -r -p -s "Remove your Yubikey, then press any key to continue..." 
 
 git checkout -b sign-root-targets
 git add ceremony/
